e31214e3752dce8a5d836fc76f58d2bea06221fe
civonulft <samuel@roording.eu>
2024-10-10 23:28:12 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,44 @@
+function links(after: number) {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+
+    stopAfter(after)
+}
+function rechts(after: number) {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+
+    stopAfter(after)
+}
+function voorruit(after: number) {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    
+    stopAfter(after)
+}
+function achteruit(after: number) {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+
+    stopAfter(after)
+}
+
+function stopAfter(after: number) {
+    basic.pause(after*1000)
+    stop()
+}
+
+function stop() {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+}
